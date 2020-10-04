31eb71203fe3670db6d78cd906b64ac0f30972c8
BigBigYu <1582240351@qq.com>
2020-10-03 17:22:12 -0700

    提交helloWorld.cpp

---
+++ tasks/week1/helloWorld.cpp
@@ -0,0 +1,10 @@
+// File Name: helloWorld.cpp
+// Author: BigBigYu
+// Created Time: Fri 02 Oct 2020 06:37:01 AM PDT
+
+#include<iostream>
+
+int main(){
+    std::cout<<"Hello world"<<std::endl;
+    return 0;
+}
